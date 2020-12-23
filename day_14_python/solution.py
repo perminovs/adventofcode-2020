00f020abdab7abdee56ae8e59aa0809ad9470cbe
perminovs <perminov-s-i@yandex.ru>
2020-12-23 22:34:16 +0500

    day 14

---
+++ day_14_python/solution.py
@@ -0,0 +1,85 @@
+from queue import Queue
+from typing import Tuple, List
+
+from day_14_python.data import MY_DATA
+
+
+def int_to_bin(num: int) -> str:
+    return bin(num)[2:]
+
+
+def bin_to_int(num: str) -> int:
+    return int(f'0b{num}', 2)
+
+
+def apply_mask(mask: str, number: int) -> int:
+    bin_num = bin(number)[2:].rjust(len(mask), '0')
+    wide_mask = mask.rjust(len(bin_num), 'X')
+    new_bin = ''.join(d if m == 'X' else m for d, m in zip(bin_num, wide_mask))
+    return bin_to_int(new_bin)
+
+
+def apply_mask2(mask: str, number: str) -> str:
+    bin_num = number.rjust(len(mask), '0')
+    wide_mask = mask.rjust(len(bin_num), '0')
+    return ''.join(d if m == '0' else m for d, m in zip(bin_num, wide_mask))
+
+
+def resolve_floating(number: str) -> List[int]:
+    q = Queue()
+    q.put(number)
+    options = []
+    while not q.empty():
+        n: str = q.get()
+        if 'X' not in n:
+            options.append(bin_to_int(n))
+            continue
+
+        q.put(n.replace('X', '0', 1))
+        q.put(n.replace('X', '1', 1))
+
+    return options
+
+
+def parse_mask(row: str) -> str:
+    return row.replace('mask = ', '')
+
+
+def parse_insert(row: str) -> Tuple[int, int]:
+    # import re? never heard
+    left = row.replace('mem[', '')
+    bracket_idx = left.rfind(']')
+    idx = int(left[:bracket_idx])
+    num = int(left[bracket_idx + 1:].replace(' = ', ''))
+    return idx, num
+
+
+def solution(raw_input: str) -> int:
+    memory = {}
+    mask = None
+    for row in (r for r in raw_input.split('\n') if r):
+        if row.startswith('mask'):
+            mask = parse_mask(row)
+            continue
+        idx, num = parse_insert(row)
+        memory[idx] = apply_mask(mask, num)
+    return sum(memory.values())
+
+
+def solution2(raw_input: str) -> int:
+    memory = {}
+    mask = None
+    for row in (r for r in raw_input.split('\n') if r):
+        if row.startswith('mask'):
+            mask = parse_mask(row)
+            continue
+        idx, num = parse_insert(row)
+        floating_idx = apply_mask2(mask, int_to_bin(idx))
+        for new_idx in resolve_floating(floating_idx):
+            memory[new_idx] = num
+    return sum(memory.values())
+
+
+if __name__ == '__main__':
+    print(solution(MY_DATA))
+    print(solution2(MY_DATA))
---
+++ day_14_python/tests.py
@@ -0,0 +1,89 @@
+import pytest
+
+from day_14_python.solution import apply_mask, solution, parse_mask, parse_insert, apply_mask2, int_to_bin, \
+    resolve_floating, solution2
+
+TEST_INPUT = """
+mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0
+"""
+
+TEST_INPUT2 = """
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+"""
+
+
+@pytest.mark.parametrize(
+    ('mask', 'number', 'expected'),
+    [
+        (
+            'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+            11,
+            73,
+        ),
+        (
+            'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+            101,
+            101,
+        ),
+        (
+            'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X',
+            0,
+            64,
+        ),
+    ],
+)
+def test_apply_mask(mask, number, expected):
+    assert apply_mask(mask, number) == expected
+
+
+def test_parse_mask():
+    assert parse_mask('mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X') == 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X'
+
+
+@pytest.mark.parametrize(
+    ('row', 'expected'),
+    [
+        ('mem[74] = 101', (74, 101)),
+        ('mem[7] = 101', (7, 101)),
+        ('mem[7] = 0', (7, 0)),
+    ],
+)
+def test_parse_insert(row, expected):
+    assert parse_insert(row) == expected
+
+
+def test_solution():
+    assert solution(TEST_INPUT) == 165
+
+
+@pytest.mark.parametrize(
+    ('mask', 'number', 'expected'),
+    [
+        (
+            '000000000000000000000000000000X1001X',
+            int_to_bin(42),
+            '000000000000000000000000000000X1101X',
+        ),
+        (
+            '00000000000000000000000000000000X0XX',
+            int_to_bin(26),
+            '00000000000000000000000000000001X0XX',
+        ),
+    ],
+)
+def test_apply_mask2(mask, number, expected):
+    assert apply_mask2(mask, number) == expected
+
+
+def test_resolve_floating():
+    assert resolve_floating('000000000000000000000000000000X1101X') == [26, 27, 58, 59]
+
+
+def test_solution2():
+    assert solution2(TEST_INPUT2) == 208
